cfd1fec7fe1a24268d5b207a790cc2d1092145ab
AJ Gambino <66279789+ajGambino@users.noreply.github.com>
2023-07-20 11:50:15 -0400

    boilerplate components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Draft from './components/Draft';
+import Live from './components/Live';
+import Login from './components/Login';
+import Lobby from './components/Lobby';
+
 
 function App() {
   return (
-    <div className="App">
-      Draft
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/live" element={<Live />} />
+        <Route path="/draft" element={<Draft />} />
+        <Route path="/lobby" element={<Lobby />} />
+      </Routes>
+    </Router>
   );
 }
 
